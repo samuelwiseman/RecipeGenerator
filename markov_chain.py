5ffbf66eba9a5f4874e378b3075090d85f6be93e
Samuel Wiseman <samuellaurencewiseman@gmail.com>
2024-04-29 15:22:32 +0100

    add probabilistic aspect to markov chain

--- markov_chain.py
+++ markov_chain.py
@@ -1,28 +1,30 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 import random
-
 
 class MarkovChain:
     def __init__(self):
-        # Initialising transitions dictionary with defaultdict
-        self.transitions = defaultdict(list)
+        # Initialising transitions dictionary with defaultdict of Counter
+        self.transitions = defaultdict(Counter)
 
     def train(self, recipes):
-        # Train the Markov Chain model using a list of recipes.
+        # Train the Markov Chain model using a list of recipes
         for recipe in recipes:
-            # Iterate over each recipe in the list of recipes
             for i in range(len(recipe) - 1):
-                # Iterate over each ingredient in the recipe
-                self.transitions[recipe[i]].append(recipe[i + 1])
-                # Update transitions dictionary with ingredient transitions
+                # Update transitions dictionary with ingredient transitions and frequencies
+                self.transitions[recipe[i]][recipe[i + 1]] += 1
 
     def suggest_next_ingredient(self, ingredient):
-        # Suggest the next ingredient based on the given ingredient.
-        possible_ingredients = self.transitions.get(ingredient, [])
-        # Get possible next ingredients for the given ingredient
+        # Suggest the next ingredient based on the given ingredient using weighted probability
+        possible_ingredients = self.transitions.get(ingredient, None)
         if not possible_ingredients:
-            # If no possible next ingredients found
             return None
-            # Return None
-        return random.choice(possible_ingredients)
-        # Return a random choice from the list of possible compatible ingredients
+        
+        # Total occurrences of subsequent ingredients to form probability distribution
+        total = sum(possible_ingredients.values())
+        # Randomly select an ingredient based on the distribution of occurrences
+        r = random.uniform(0, total)
+        cumulative = 0
+        for item, count in possible_ingredients.items():
+            cumulative += count
+            if r < cumulative:
+                return item
